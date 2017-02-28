1981026e0f148a5438a4ae1e4951501b70749204
Tanner Embry <awesomete@gmail.com>
2017-02-27 16:15:21 -0800

    stuff kinda works now maybe

--- ProbC.py
+++ ProbC.py
@@ -1,55 +1,76 @@
 from random import gammavariate, seed
-import simpy
+from SimPy.Simulation import *
 
-# class glb:
-#     env = simpy.Environment()
-#     # Start the run process everytime an instance is created.
-#     # action = process(self.run())
+class glb:
+	order_list = [] #list of times that orders took
+	delivery_list = [] #amount of time delivery takes
+
+	stock = None
+	order_amount = 1
+	delivery_amount = 1
 
 
-
-class Customer():
-	def custtime(alphac,betac):
+class Customer(Process):
+	def custtime(self, alphac,betac):
 		while True:
-			order_list = [] #list of times that orders took
+
 			order_time = random.gammavariate(alphac,betac) #returns gamma distribution--for time customer orders arrive
-			order_list.append(order_time)
-			order_amount = 1.0 # amount requested
-			yield get, stock, order_amount
-			print 'at %7.4f, add %7.4f units, now amount = %6.4f'\
-	               %(now(),order_amount,stock.amount)
-	        yield hold, order_time
+
+	  		if glb.stock.amount > 0 : # if stock amount > 0, fulfill order instantly
+				yield get, self, glb.stock, glb.order_amount
+				glb.order_list.append(0) # 0 represents an instant order
+
+				print 'at %7.4f, get %7.4f units, now amount = %6.4f'\
+					%(now(),glb.order_amount,glb.stock.amount)
+
+			else: # otherwise, fulfill order in the calculated time
+				yield hold, self, order_time
+
+				glb.order_list.append(order_time)
+
+				print 'at %7.4f, get %7.4f units, now amount = %6.4f'\
+					%(now(),glb.order_amount,glb.stock.amount)
 
 
 
-class Inventory():
-	def inventime(alphai,betai):
+class Inventory(Process):
+	def inventime(self, alphai,betai):
 		while True:
-			delivery_list = [] #amount of time delivery takes
 			delivery_time = random.gammavariate(alphai,betai) # returns gamma distribution--for time to deliver new inventory
-			delivery_list.append(delivery_time)
-			delivery_amount = 1.0 # amount of items delivered
-			yield put, stock, deliver_amount
+			glb.delivery_list.append(delivery_time)
+			
+			yield put, self, glb.stock, glb.delivery_amount
 			print 'at %7.4f, add %7.4f units, now amount = %6.4f'\
-	               %(now(),delivery_amount,stock.amount)
-			yield hold, delivery_time
+	               %(now(),glb.delivery_amount,glb.stock.amount)
+			yield hold, self, delivery_time
 
 
 def storesim(maxsimtime, alphac, betac, alphai, betai):
-	#maxsimtime gets passed in and determines how long whole simulation runs for
-	stock = Level(monitored=true)
 
-	seed(99999)
+	glb.stock = Level(monitored=True)
+
+	seed(12345)
 
 	initialize()
 
+	# activate both the store and the customer simulations
 	store = Inventory()
 	activate (store, store.inventime(alphai,betai))
+	
 	customer = Customer()
 	activate (customer, customer.custtime(alphac, betac))
 
 	simulate (until = maxsimtime)
 
+	# add up the total order times of all orders
+	order_sum = 0
+	instant_orders = 0
+	for i in glb.order_list:
+		if i == 0: # count the total amount of instant orders
+			instant_orders = instant_orders + 1
+		order_sum = order_sum + i
 
-	
-# TODO possible interrupts?#
+	mean_ordertime = order_sum / len(glb.order_list)
+	inst_prop = float(instant_orders) / float(len(glb.order_list))
+
+	return (mean_ordertime, inst_prop, 0) # TODO: need to compute last variable in tuple
---
+++ test.py
@@ -0,0 +1,5 @@
+import ProbC
+
+x = ProbC.storesim(10000,2,2,2,2)
+
+print x
